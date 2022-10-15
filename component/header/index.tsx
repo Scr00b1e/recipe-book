913a0ce35163254386d97e8d493f12e3a7b68f45
Scr00b1e <kbolot312@gmail.com>
2022-10-15 22:18:27 +0600

    init

---
+++ component/header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './header.module.scss'
+
+export const Header: React.FC = () => {
+  return (
+    <div className={styles.header}>
+        Header
+    </div>
+  )
+}
