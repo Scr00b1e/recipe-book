185f7093ea8583ff58e734aa5b7b25cf1989c729
Scr00b1e <kbolot312@gmail.com>
2022-10-16 14:29:17 +0600

    added header

--- component/header/index.tsx
+++ component/header/index.tsx
@@ -4,7 +4,43 @@ import styles from './header.module.scss'
 export const Header: React.FC = () => {
   return (
     <div className={styles.header}>
-        Header
+      <div className={styles.header__inner}>
+        <div className={styles.header__left}>
+          <div className={styles.header__name}>
+            <img 
+              src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png" 
+              alt=""
+              className={styles.header__img} />
+              <h1 className={styles.header__title}>
+                Recipes
+              </h1>
+          </div>
+          <nav className={styles.header__nav}>
+            <div className={styles.header__item}>
+              sup
+            </div>
+            <div className={styles.header__item}>
+              sup
+            </div>
+            <div className={styles.header__item}>
+              sup
+            </div>
+            <div className={styles.header__item}>
+              sup
+            </div>
+            <div className={styles.header__item}>
+              sup
+            </div>
+            <div className={styles.header__item}>
+              sup
+            </div>
+          </nav>
+        </div>
+        <div className={styles.header__right}>
+          <img src="../favorite.svg" alt="" />
+          <img src="../search.svg" alt="" />
+        </div>
+      </div>
     </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-
+import './global.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
