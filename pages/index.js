80be27d33074934b854fc8f23dd4d17dfbdead2c
Scr00b1e <kbolot312@gmail.com>
2022-10-20 23:37:06 +0600

    added items

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import { Header } from "../component/header/index"
 import Intro from "../component/intro/Intro"
+import Item from "../component/item/Item"
 
   const Home = () => (
     <>
       <Header/>
       <Intro/>
+      <div className="container">
+        <h1 className="heading__title">Go there!</h1>
+        <section className="heading">
+          <Item/>
+        </section>
+      </div>
     </>
   )
 
