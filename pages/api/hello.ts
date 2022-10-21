2694f7761facecced387583385dc2dbfcf9a2685
Scr00b1e <kbolot312@gmail.com>
2022-10-21 19:48:41 +0600

    managed api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { fastNquick } from './data/fastNquick';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(fastNquick)
 }
