9d6ef10422d939b33e91862f48b5cbc949f23a16
Scr00b1e <kbolot312@gmail.com>
2022-10-24 21:42:01 +0600

    clear
    cancel

--- component/item/index.tsx
+++ component/item/index.tsx
@@ -5,16 +5,17 @@ export type RecipesProps = {
   id: string
   img: string
   title: string
+  time: number
 }
 
-export const Item: React.FC<RecipesProps> = ({ img, title }) => {
+export const Item: React.FC<RecipesProps> = ({ img, title, time }) => {
   return (
     <div className={styles.item}>
         <img src={img} alt="" className={styles.item__img}/>
         <span className={styles.item__type}>item type</span>
         <h1 className={styles.item__title}>{title}</h1>
         <div className={styles.item__bottom}>
-            <p className={styles.item__text}>item done</p>
+            { time && <p className={styles.item__text}>{time} mins</p> }
         </div>
     </div>
   )
--- pages/api/data/breakfast.ts
+++ pages/api/data/breakfast.ts
@@ -1,7 +1,19 @@
 export const breakfast = [
     {
         "id": "0",
-        "title": "Hawaiian Roll Sliders with Meat Sauce and Parm",
-        "img": "https://www.simplyrecipes.com/thmb/HBtBhymlFjjKoO8JGqs5bu5NaQU=/390x260/filters:max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Meatball-Parm-Sliders-LEAD-5-837a559ccfab4a8ba861db245375a5f8.jpg"
+        "title": "Lion House Rolls",
+        "img": "https://www.simplyrecipes.com/thmb/O33rNvqGuR-SQb-dZOMzeJp8uZ4=/390x260/filters:max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2020__05__Lion-House-Rolls-LEAD-09-3f9c16a0767542e4b92e4b5b06270b94.jpg",
+        "time": 80
+    },
+    {
+        "id": "1",
+        "title": "6 tips for better dinner rolls",
+        "img": "https://www.simplyrecipes.com/thmb/wQJ4uM4pbDBjqpj-orQZ8UVuIAA=/390x260/filters:max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Dinner-Roll-Tips-METHOD-03-d55a0bba96cb472dbd3b69c10289c3c7.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Milk bread rolls",
+        "img": "https://www.simplyrecipes.com/thmb/xnEH1EVD3wUFlS7zQySbA3wRkbo=/390x260/filters:max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Milk-Bread-Rolls-LEAD-01-cc98ed9221e046a88534cde561ff776f.jpg",
+        "time": 45
     }
 ]
