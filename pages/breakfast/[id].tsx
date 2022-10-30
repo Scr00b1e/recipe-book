ef70ad614afec2321101e399a933bfe47178b0aa
Scr00b1e <kbolot312@gmail.com>
2022-10-30 20:10:49 +0600

    did some small changes

--- pages/breakfast/[id].tsx
+++ pages/breakfast/[id].tsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import styles from './breakfast.module.scss'
 
-export async function getServerSideProps(context) {
+export const getStaticPaths = async () => {
+  const res = await fetch(`${process.env.API_HOST}/brPage`)
+  const data = await res.json()
+
+  const paths = data.map(({ id }) => ({
+    params: { id: id.toString() }
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps(context) {
   try {
       const { id } = context.params
       const responce = await fetch(`${process.env.API_HOST}/brPage/${id}`)
@@ -24,13 +38,15 @@ export async function getServerSideProps(context) {
 }
 
 type ItemProps = {
-  title: string
-  time: number
-  id: string
-  img: string
+  item: {
+    title: string
+    time: number
+    id: string
+    img: string
+  }
 }
 
-const BreakfastItem = ({item}) => {
+const BreakfastItem: React.FC<ItemProps> = ({item}) => {
   // const things = item.map((obj) => (
   //   {...obj}
   // ))
