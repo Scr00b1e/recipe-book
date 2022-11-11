9278abce0298a76df6feef67b5460a86520327f3
Scr00b1e <kbolot312@gmail.com>
2022-11-11 22:27:36 +0600

    worked with redux

--- component/item/index.tsx
+++ component/item/index.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { addItem, removeItem } from '../../redux/slices/favoritesSlice'
 import styles from './item.module.scss'
 
 export type RecipesProps = {
@@ -12,19 +14,37 @@ export type RecipesProps = {
 }
 
 export const Item: React.FC<RecipesProps> = ({ 
-  id, 
-  img, 
-  title, 
-  time, 
-  type, 
-  apiUrl }) => {
+    id, 
+    img, 
+    title, 
+    time, 
+    type, 
+    apiUrl }) => {
     
+    const dispatch = useDispatch()
+    const [clicked, setClicked] = React.useState(false)
+    const onClickFav = () => {
+      const item = {
+        id,
+        img,
+        title,
+        time,
+        type
+      }
+      dispatch(addItem(item))
+      setClicked(!clicked)
+      if(clicked) {
+       dispatch(removeItem(id) )
+      }
+    }
+
   return (
     <Link href={`/${apiUrl}/${id}`}>
       <div className={styles.item}>
         <div className={styles.item__image}>
         <img src={img} alt="" className={styles.item__img}/>
-        <img src="../favorite.svg" alt="" className={styles.item__heart}/>
+        <img src={clicked ? "../favorite-click.png" : "../favorite.svg"} alt="" className={styles.item__heart}
+        onClick={onClickFav}/>
         </div>
         <span className={styles.item__type}>{type}</span>
         <h1 className={styles.item__title}>{title}</h1>
--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import FavItem, { FavProps } from '../../component/favItem/FavItem'
+import { selectFav } from '../../redux/slices/favoritesSlice'
 import styles from './favorites.module.scss'
 
 const Favorites: React.FC = () => {
+  const items = useSelector(selectFav)
   return (
     <div className={styles.favorite}>
       <div className='container'>
         <h1 className={styles.favorite__title}>
-          hello world
+          Clear All
         </h1>
+        <div className={styles.favorite__content}>
+          {
+            Object.values(items).map((obj: FavProps) => (
+              <FavItem {...obj} key={obj.id}/>
+            ))
+          }
+        </div>
       </div>
     </div>
   )
