dcbf768d4796ed22873f4579cc00a5594fff3be6
Scr00b1e <kbolot312@gmail.com>
2022-11-17 20:56:25 +0600

    updated FavItem

--- component/favItem/FavItem.tsx
+++ component/favItem/FavItem.tsx
@@ -1,4 +1,8 @@
+// import Link from 'next/link'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { removeItem } from '../../redux/slices/favoritesSlice'
+import styles from '../item/item.module.scss'
 
 export type FavProps = {
   id: string
@@ -9,10 +13,29 @@ export type FavProps = {
 }
 
 const FavItem: React.FC<FavProps> = ({ id, title, type, img, time }) => {
+  const dispatch = useDispatch()
+  const onClickRemove = () => {
+    dispatch(removeItem(id))
+  }
+
   return (
-    <div>
-      <img src={img} alt="" />
+    // <Link href={`/${apiUrl}/${id}`}>
+    <div className={styles.item}>
+      <div className={styles.item__image}>
+        <img src={img} alt="" className={styles.item__img} />
+        <img
+          src="../favorite-click.png"
+          alt=""
+          onClick={onClickRemove}
+          className={styles.item__heart} />
+      </div>
+      <span className={styles.item__type}>{type}</span>
+      <h1 className={styles.item__title}>{title}</h1>
+      <div className={styles.item__bottom}>
+        {time && <p className={styles.item__text}>{time} mins</p>}
+      </div>
     </div>
+    // </Link>
   )
 }
 
