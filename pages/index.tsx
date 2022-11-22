6497af46da0d2c7575deadfeb559158605c2d64e
Scr00b1e <kbolot312@gmail.com>
2022-11-22 15:17:35 +0600

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,42 +3,42 @@ import { Item, RecipesProps } from "../component/item"
 import { Header } from "../component/header"
 import Link from "next/link"
 
-// export const getStaticProps = async () => {
-//   try {
-//     const responce = await fetch(`${process.env.API_HOST}/recipes`)
-//     const recipes = await responce.json()
+export const getStaticProps = async () => {
+  try {
+    const responce = await fetch(`${process.env.API_HOST}/recipes`)
+    const recipes = await responce.json()
 
-//     if(!recipes) {
-//       return {
-//         notFound: true
-//       }
-//     }
+    if (!recipes) {
+      return {
+        notFound: true
+      }
+    }
 
-//     return {
-//       props: {recipes}
-//     }
-//   } catch {
-//     alert('something is wrong')
-//   }
-// }
+    return {
+      props: { recipes }
+    }
+  } catch {
+    alert('something is wrong')
+  }
+}
 
 const Home = ({ recipes }) => (
   <>
     <Header />
-    {/* <Intro/>
-      <div className="container">
-        <Link href={'/breakfast'}>
-          <h1 className="heading__title">Go there!</h1>
-        </Link>
-        <section className="heading">
-          {
-           recipes && 
-           recipes.map((obj: RecipesProps) => (
-            <Item {...obj} key={obj.id} apiUrl={'/recipes'}/> 
+    <Intro />
+    <div className="container">
+      <Link href={'/breakfast'}>
+        <h1 className="heading__title">Go there!</h1>
+      </Link>
+      <section className="heading">
+        {
+          recipes &&
+          recipes.map((obj: RecipesProps) => (
+            <Item {...obj} key={obj.id} apiUrl={'/recipes'} />
           ))
-          }
-        </section>
-      </div> */}
+        }
+      </section>
+    </div>
   </>
 )
 
